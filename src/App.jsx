632e890dbf971e0c9c06522bc21088602e67329c
SaiKalpana Reddy <saireddy7991@gmail.com>
2025-07-28 16:04:40 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,18 @@ function App() {
   const inputRef = useRef(null);
 
   useEffect(() => {
-    inputRef.current.value = "0"
-  },[])
+    inputRef.current.value = "0";
+  }, []);
 
   const handleClick = () => {
-    inputRef.current.value++
+    let count = parseInt(inputRef.current.value);
+    console.log(count, "count");
+    let count1 = count + 1;
+    console.log(count1, "count1");
+    count1 = count1.toString();
+    let count2 = parseInt(count1);
+    count2 = count2 + 1;
+    console.log(count2, "count2");
   };
 
   return (
